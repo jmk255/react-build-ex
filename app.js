a856c1f5e31eebc83a3ec71db445a2c26e4dee9d
jmk255 <jmk255@naver.com>
2024-10-22 15:50:36 +0900

    3

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express();
 
 app.set("port", process.env.PORT || 5000);
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "react-build/build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
+  res.sendFile(path.join(__dirname, "/react-build/build/index.html"));
 });
 
 app.listen(app.get("port"), () => {
